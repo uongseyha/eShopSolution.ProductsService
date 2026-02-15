7d052b42eff2237959f8cc1ce78b4d708bc9c2c1
Seyha Uong <uong.seyha@gmail.com>
2026-02-15 18:05:22 -0500

    Implement Docker Compose and Seed

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -13,8 +13,13 @@ namespace eShop.ProductsService.DataAccessLayer
     {
       //TO DO: Add Data Access Layer services into the IoC container
 
+      string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+      string connectionString = connectionStringTemplate
+        .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+        .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
+
       services.AddDbContext<ApplicationDbContext>(options => {
-        options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+        options.UseMySQL(connectionString);
       });
 
       services.AddScoped<IProductsRepository, ProductsRepository>();
